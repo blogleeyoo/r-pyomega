5aeb016f84e9fb07d176a7b373da21f909654053
seongjaeyoo <seong1898@gmail.com>
2021-02-02 22:40:56 +0900

    hotfix

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
----
-layout: compress
----
-
-/* Registering Service Worker */
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('{{ "/sw.js" | relative_url }}');
-};
+/* Registering Service Worker */ if('serviceWorker' in navigator) { navigator.serviceWorker.register('/sw.js'); };
